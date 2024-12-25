7a1f0d3de0289a7f33338e64ca05402e9f180cc7
nuyhus-m <minsh2344@gmail.com>
2024-12-25 23:07:25 +0900

    [주간] 백준_풍선 맞추기

---
+++ 1204/04/nuyhus.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class nuyhus {
+
+    static int[] arr = new int[1_000_001];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int h = Integer.parseInt(st.nextToken());
+            if (arr[h] > 0) {
+                arr[h]--;
+            }
+            arr[h - 1]++;
+        }
+
+        int ans = 0;
+        for (int i : arr) {
+            ans += i;
+        }
+
+        System.out.println(ans);
+    }
+}
