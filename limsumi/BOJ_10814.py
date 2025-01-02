f0783e2bf41b60b7102d0ae1542125914003a247
임수미 <114466348+LimSumi@users.noreply.github.com>
2025-01-02 23:12:52 +0900

    [금] 백준_나이순 정렬

---
+++ limsumi/BOJ_10814.py
@@ -0,0 +1,13 @@
+N=int(input())
+
+person=[]
+
+for i in range(N):
+    age,name=list(map(str, input().split()))
+
+    person.append([int(age),i, name])
+
+person.sort()
+
+for i in range(N):
+    print(str(person[i][0])+" "+person[i][2])
