299ecc969cbb055fd12af2ad0a1c1f3fc1b9f660
minsh2344 <minsh2344@gmail.com>
2025-01-09 16:13:50 +0900

    수정

--- nuyhus/BOJ_3190.java
+++ nuyhus/BOJ_3190.java
@@ -1,108 +1,96 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class BOJ_3190 {
 
-    static int n, k, l;
+    static int boardSize, appleSize, changeSize;
+    static int[][] appleInfo;
+    static HashMap<Integer, String> changeInfo;
     static int[][] board;
-    static int[] head;
-    static int[] tail;
-    static int[][] apple;
-    static String[][] change;
-    static int[][] d = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    static int[] head = new int[2];
+    static int[] tail = new int[2];
+    static int[][] d = {{0, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        n = sc.nextInt();
-        k = sc.nextInt();
-        board = new int[n][n];
-        apple = new int[k][2];
-        for (int i = 0; i < k; i++) {
+        boardSize = Integer.parseInt(br.readLine());
+        board = new int[boardSize][boardSize];
+
+        appleSize = Integer.parseInt(br.readLine());
+        appleInfo = new int[appleSize][2];
+        for (int i = 0; i < appleSize; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 2; j++) {
-                apple[i][j] = sc.nextInt() - 1;
+                appleInfo[i][j] = Integer.parseInt(st.nextToken()) - 1;
             }
         }
-        l = sc.nextInt();
-        change = new String[l][2];
-        for (int i = 0; i < l; i++) {
-            for (int j = 0; j < 2; j++) {
-                change[i][j] = sc.next();
-            }
+
+        changeSize = Integer.parseInt(br.readLine());
+        changeInfo = new HashMap<>();
+        for (int i = 0; i < changeSize; i++) {
+            st = new StringTokenizer(br.readLine());
+            int time = Integer.parseInt(st.nextToken());
+            String direction = st.nextToken();
+            changeInfo.put(time, direction);
         }
 
         board[0][0] = 1;
-        head = new int[2];
-        tail = new int[2];
         go(0, 0, 1);
 
         System.out.println(cnt);
     }
 
-    static void go(int x, int y, int idx) {
-
-        // 시간 증가
+    static void go(int x, int y, int dir) {
         cnt++;
 
-        // 이동
-        int nx = x + d[idx][0];
-        int ny = y + d[idx][1];
+        int nx = x + d[dir][0];
+        int ny = y + d[dir][1];
 
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] != 0) {
+        if (nx < 0 || ny < 0 || nx >= boardSize || ny >= boardSize || board[nx][ny] != 0) {
             return;
         }
 
-        // 머리 이동
         head[0] = nx;
         head[1] = ny;
 
-        // 꼬리 이동
-        int tailX = tail[0];
-        int tailY = tail[1];
-
         boolean isApple = false;
-        for (int i = 0; i < k; i++) {
-            if (nx == apple[i][0] && ny == apple[i][1]) {
-                apple[i][0] = -1;
-                apple[i][1] = -1;
+        for (int i = 0; i < appleSize; i++) {
+            if (nx == appleInfo[i][0] && ny == appleInfo[i][1]) {
+                appleInfo[i][0] = -1;
+                appleInfo[i][1] = -1;
                 isApple = true;
                 break;
             }
         }
 
+        int tailX = tail[0];
+        int tailY = tail[1];
         if (!isApple) {
             tail[0] = tail[0] + d[board[tailX][tailY]][0];
             tail[1] = tail[1] + d[board[tailX][tailY]][1];
             board[tailX][tailY] = 0;
         }
 
-        // 방향
-        int nIdx = idx;
-        for (int i = 0; i < l; i++) {
-            if (Integer.parseInt(change[i][0]) == cnt) {
-                if (change[i][1].equals("D")) {
-                    nIdx += 1;
-                    if (nIdx == 5) {
-                        nIdx = 1;
-                    }
-                } else {
-                    nIdx -= 1;
-                    if (nIdx == 0) {
-                        nIdx = 4;
-                    }
+        if (changeInfo.containsKey(cnt)) {
+            String temp = changeInfo.get(cnt);
+            if (temp.equals("D")) {
+                dir++;
+                if (dir == 5) {
+                    dir = 1;
                 }
-
-                break;
-            }
-
-            if (Integer.parseInt(change[i][0]) > cnt) {
-                break;
+            } else if (temp.equals("L")) {
+                dir--;
+                if (dir == 0) {
+                    dir = 4;
+                }
             }
         }
 
-        board[nx][ny] = nIdx;
-        go(nx, ny, nIdx);
+        board[nx][ny] = dir;
+        go(nx, ny, dir);
     }
 }
