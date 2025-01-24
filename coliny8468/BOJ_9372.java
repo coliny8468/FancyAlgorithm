6a6230d43108840af6ee44763cf6ed8fa0e6453e
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-01-24 23:46:04 +0900

    [금] 백준_상근이의 여행
    엄청쉬움 추천합니다.

---
+++ coliny8468/BOJ_9372.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+
+            for (int j = 0; j < m; j++) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+            }
+            System.out.println(n-1);
+
+        }
+
+    }
+
+}
