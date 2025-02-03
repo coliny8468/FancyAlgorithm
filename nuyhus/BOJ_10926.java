4b283cdf31d18af92ecf03b33f7709c75ec34fe6
민수현 <minsh2344@gmail.com>
2025-02-03 23:16:21 +0900

    [월] 백준_??!

---
+++ nuyhus/BOJ_10926.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_10926 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine() + "??!");
+    }
+}
