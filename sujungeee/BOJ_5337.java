4c5598ccc4a973cc8c1f0d7ef928b1af529a76bb
남수정 <2020113388@dgu.ac.kr>
2025-03-17 21:45:42 +0900

    [월] 백준_웰컴

---
+++ sujungeee/BOJ_5337.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/5337
+// 5337: 웰컴
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_5337 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
