edb2359a48d22c0c68ff9b6522b2685a89e6c7d3
임수미 <114466348+LimSumi@users.noreply.github.com>
2025-03-20 19:41:30 +0900

    [주간] 백준_비밀 모임

---
+++ 0304/limsumi.py
@@ -0,0 +1,45 @@
+T=int(input())
+
+for i in range(T):
+    INF = int(1e9)
+
+    n,m = map(int, input().split())
+
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if a == b:
+                graph[a][b] = 0
+
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a][b] = c
+        graph[b][a] = c
+
+    for k in range(1, n + 1):
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+                graph[b][a] = min(graph[b][a], graph[k][a] + graph[b][k])
+
+                graph[a][b]=min(graph[a][b], graph[b][a])
+                graph[b][a]=min(graph[a][b], graph[b][a])
+
+    p=int(input())
+
+
+    friends=list(map(int, input().split()))
+
+    maxx=2e9
+    maxxIndex=0
+    for q in range(1,n+1):
+        summ=0
+        for w in friends:
+            summ+=graph[w][q]
+        if summ<maxx:
+            maxxIndex=q
+            maxx=summ
+
+
+    print(maxxIndex)
