beda902c3516d465b4bfae4f29ff4140c29c594b
남수정 <2020113388@dgu.ac.kr>
2025-03-27 09:04:13 +0900

    [목] 백준_Can you add this?

---
+++ sujungeee/BOj_7891.java
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/7891
+// 7891: Can you add this?
+package com.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_7891 {
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a+b);
+        }
+    }
+}
