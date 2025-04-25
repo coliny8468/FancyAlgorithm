3320774f48f19ca6cdb550740facb4b41b419eae
남수정 <2020113388@dgu.ac.kr>
2025-04-26 00:16:30 +0900

    [금]백준_The World Responds

---
+++ sujungeee/BOJ_23234.java
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/23234
+// 23234: The World Responds
+
+public class BOJ_23234 {
+
+    public static void main(String[] args) {
+        System.out.println("The world says hello!");
+    }
+}
