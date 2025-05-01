dd8c6a739bf09497848deec96f7f834bb10fbf0a
임수미 <114466348+LimSumi@users.noreply.github.com>
2025-05-01 23:28:30 +0900

    [목] 백준_알약

--- limsumi/BOJ_4811.py
+++ limsumi/BOJ_4811.py
@@ -1,20 +1,20 @@
-def yak(n):
-    dp = [[0] * 31 for i in range(31)]
-    dp[0][0] = 1
+    def yak(n):
+        dp = [[0] * 31 for i in range(31)]
+        dp[0][0] = 1
+        
+        for i in range(n+1):
+            for j in range(i + 1):
+                if i < n:
+                    dp[i + 1][j] += dp[i][j]
+                if j < i:
+                    dp[i][j + 1] += dp[i][j]
+        
+        return dp[n][n]
     
-    for i in range(n+1):
-        for j in range(i + 1):
-            if i < n:
-                dp[i + 1][j] += dp[i][j]
-            if j < i:
-                dp[i][j + 1] += dp[i][j]
+    while True:
+        n=int(input())
     
-    return dp[n][n]
-
-while True:
-    n=int(input())
-
-    if n==0:
-        break
-    
-    print(yak(n))
+        if n==0:
+            break
+        
+        print(yak(n))
