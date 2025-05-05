9a723438ac3c26f33d8fc573ea49aa1be0b3d12a
남수정 <2020113388@dgu.ac.kr>
2025-05-05 23:37:09 +0900

    4월 5주차 데일리 문제 취합

---
+++ sujungeee/BOJ_2752.java
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/2752
+// 2752: 세수정렬
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < 3; i++) {
+            if (i == 2) {
+                sb.append(arr[i]);
+            } else {
+                sb.append(arr[i]).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
