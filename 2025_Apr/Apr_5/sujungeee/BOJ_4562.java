ff56fb9bf5fdeda77bf6815649b76cc50afb5277
남수정 <2020113388@dgu.ac.kr>
2025-05-05 23:39:30 +0900

    4월 5주차 데일리문제 파일정리

--- 2025_Apr/Apr_5/sujungeee/BOJ_4562.java
+++ 2025_Apr/Apr_5/sujungeee/BOJ_4562.java
@@ -9,6 +9,7 @@ public class BOJ_4562 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
