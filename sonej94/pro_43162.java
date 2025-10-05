58a1fb5236ed67bd817480ba3a07ac9cf68126c2
sonej94 <sonej94113@naver.com>
2025-10-06 02:32:49 +0900

    [1] 네트워크

---
+++ sonej94/pro_43162.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, visited, n);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    private void dfs(int cur, int[][] g, boolean[] visited, int n) {
+        visited[cur] = true;
+        for (int next = 0; next < n; next++) {
+            if (next == cur) continue;
+            if (g[cur][next] == 1 && !visited[next]) {
+                dfs(next, g, visited, n);
+            }
+        }
+    }
+}
