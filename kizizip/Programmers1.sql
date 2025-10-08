834602097060081d7f99612eb3cbdf748a1d279e
kizizip <163630533+kizizip@users.noreply.github.com>
2025-10-08 21:13:12 +0900

    [1] 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기

---
+++ kizizip/Programmers1.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT car_type, count(*) as cars
+from car_rental_company_car
+WHERE options LIKE '%가죽시트%'
+   OR options LIKE '%통풍시트%'
+   OR options LIKE '%열선시트%'
+group by car_type
+order by car_type asc
