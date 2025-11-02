9e7895dcff80c2d8ed9cbfeca3ed9eb5267e42cd
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-11-03 01:02:52 +0900

    []
    Implement solution method to filter and sort array by divisor.

---
+++ coliny8468/programmers3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i =0; i<arr.length ; i++){
+            if(arr[i]% divisor ==0 ){
+                answer.add(arr[i]);
+            }
+        }
+        
+       Collections.sort(answer);
+        
+        if(answer.size() == 0){
+            answer.add(-1);
+        }
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+        
+        return result;
+    }
+}
