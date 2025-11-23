754d9c085bde4798b7b58a705d3b122e24d6b505
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-11-24 02:10:33 +0900

    [3] 가운데 글자 가져오기

---
+++ coliny8468/programmers3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        
+        if (s.length() % 2 == 0) {
+            return "" + s.charAt(s.length()/2 - 1) + s.charAt(s.length()/2);
+        } else {
+            return String.valueOf(s.charAt(s.length()/2));
+        }
+    }
+}
